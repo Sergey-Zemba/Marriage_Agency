dd9ac4e25cc7ff313563d5636577d9d143eed6f7
Sergey-Zemba <sergey.zemba@gmail.com>
2016-03-02 20:24:38 +0200

    Saving into db 2

---
+++ Marriage Agency(Women)/Models/Characteristics/PersonalData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Marriage_Agency_Women_.Models.Characteristics
+{
+    public abstract class PersonalData
+    {
+        public PersonalData(int position, string russian, string english, string japanese)
+        {
+            Position = position;
+            RussianName = russian;
+            EnglishName = english;
+            JapaneseName = japanese;
+        }
+        public int Id { get; set; }
+        public int Position { get; set; }
+        public string RussianName { get; set; }
+        public string EnglishName { get; set; }
+        public string JapaneseName { get; set; }
+    }
+}
